757002fe92cfbbe46db1edbc424909dc12cb6064
yamamoto-kai <kai.yamamoto.howcollect@gmail.com>
2025-12-23 17:21:13 +0900

    調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useMemo, useState } from 'react'
-import { Trash } from 'lucide-react';
+import { useMemo, useState } from 'react';
+import { Trash, Undo2 } from 'lucide-react';
 import './App.css'
 
 type Todo = {
@@ -9,10 +9,12 @@ type Todo = {
   deleted: boolean;
 }
 
+type Filter = "all" | "active" | "completed" | "deleted";
+
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [inp, setInp] = useState<string>("");
-  const [option, setOption] = useState<string>("all");
+  const [option, setOption] = useState<Filter>("all");
 
   const handleAddTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,29 +31,29 @@ function App() {
     setInp("");
   };
 
-  const handleDeleteTodo = (id :string) => {
+  const handleToggleDelete = (id :string) => {
     setTodos(todos => 
       todos.map(todo => 
         todo.id === id ?
-          { ...todo, deleted: true } : todo
+          { ...todo, deleted: !todo.deleted } : todo
       )
     );
   };
 
-  const handleCompleteTodo = (id: string) => {
+  const handleToggleComplete = (id: string) => {
     setTodos(todos =>
-      todos.map((todo) =>
+      todos.map(todo =>
         todo.id === id ?
-          { ...todo, completed: true } : todo
+          { ...todo, completed: !todo.completed } : todo
       )
     );
   };
 
-  const handleEditTodo = (id: string,content: string) => {
+  const handleEditTodo = (id: string, content: string) => {
     setTodos(todos =>
-      todos.map((todo) => 
-        todo.id === id ? 
-        {...todo, content: content} : todo        
+      todos.map(todo =>
+        todo.id === id ?
+          { ...todo, content: content } : todo
       )
     );
   };
@@ -75,10 +77,10 @@ function App() {
           <h1 className='text-3xl font-medium'>TODO APP</h1>
           <form className='flex w-2/3 gap-3' onSubmit={handleAddTodo}>
             <input className="flex-1 p-3 px-4 bg-white rounded outline-none" type="text" placeholder='Add Todo Item' value={inp} onChange={(e) => setInp(e.target.value)} />
-            <button className='cursor-pointer px-4 rounded bg-black text-white hover:bg-gray-600' type='submit' disabled={inp ? false : true}>Add</button>
+            <button className='cursor-pointer px-4 rounded bg-black text-white hover:bg-gray-600' type='submit' disabled={inp.trim().length === 0}>Add</button>
           </form>
-          <div>
-            <select value={option} onChange={(e)=> setOption(e.target.value)}>
+          <div className='flex w-3/5 text-xs'>
+            <select className="bg-white outline-none rounded p-2" value={option} onChange={(e) => setOption(e.target.value as Filter)}>
               <option value="all">all</option>
               <option value="active">active</option>
               <option value="completed">completed</option>
@@ -92,9 +94,9 @@ function App() {
                 :
                 filteredTodos.map((todo: Todo) => (
                   <div key={todo.id} className='flex gap-4'>
-                    <input className='cursor-pointer' type="checkbox" checked={todo.completed} onChange={() => handleCompleteTodo(todo.id)} />
-                    <input className={`flex-1 rounded p-3 px-4 outline-none text-sm ${todo.completed ? `bg-gray-300 cursor-not-allowed text-gray-600` : `bg-white`}`} type="text" value={todo.content} onChange={e => handleEditTodo(todo.id, e.target.value)} disabled={todo.completed} />
-                    <button className='cursor-pointer' onClick={() => handleDeleteTodo(todo.id)}><Trash size={20} /></button>
+                    <input className={`${todo.deleted ? `cursor-not-allowed` : `cursor-pointer`}`} type="checkbox" checked={todo.completed} onChange={() => handleToggleComplete(todo.id)} disabled={todo.deleted} />
+                    <input className={`flex-1 rounded p-3 px-4 outline-none text-sm ${todo.completed || todo.deleted ? `bg-gray-300 cursor-not-allowed text-gray-600` : `bg-white`}`} type="text" value={todo.content} onChange={e => handleEditTodo(todo.id, e.target.value)} disabled={todo.completed || todo.deleted} />
+                    <button className='cursor-pointer' onClick={() => handleToggleDelete(todo.id)}>{todo.deleted ? <Undo2 size={20} /> : <Trash size={20} />}</button>
                   </div>
                 ))
             }
@@ -102,7 +104,7 @@ function App() {
           {
             filteredTodos.length !== 0 &&
             <div className='w-3/5 flex'>
-              <p>{filteredTodos.length} items left</p>
+              <p>{filteredTodos.length} items</p>
             </div>
           }
         </div>
